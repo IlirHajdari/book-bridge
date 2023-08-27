f0d0893b222030a5e2175a617d460d65a27b599e
Ilir <ilir.hajdari111@gmail.com>
2023-08-27 17:36:39 -0400

    corrected auth issue again

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -19,10 +19,16 @@ function getUserFromToken(token) {
 
 module.exports = {
   contextMiddleware: ({ req }) => {
-    const token = req.headers.authorization?.split(" ").pop().trim() || "";
-    const user = getUserFromToken(token);
+    let user = null; // default user to null
+    try {
+      const token = req.headers.authorization?.split(" ").pop().trim() || "";
+      user = getUserFromToken(token);
+    } catch (error) {
+      console.log("Error extracting user from token:", error.message);
+    }
     return { user };
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
