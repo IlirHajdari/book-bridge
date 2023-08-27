099eb6efb331366fa17465ebc25542d1b195396f
Ilir <ilir.hajdari111@gmail.com>
2023-08-27 17:08:52 -0400

    database changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,24 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/BookBridgeDB"
-);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/BookBridgeDB",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+// Calling the function to initiate the connection
+connectDB();
 
 module.exports = mongoose.connection;
